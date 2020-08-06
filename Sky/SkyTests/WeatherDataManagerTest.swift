ab3c6ae5bdae2cfcd6214d8c360c5bcfd03a1479
fengyuxuan2008 <453039687@qq.com>
2020-08-07 00:45:47 +0800

    为单元测试做准备

---
+++ Sky/SkyTests/WeatherDataManagerTest.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherDataManagerTest.swift
+//  SkyTests
+//
+//  Created by apple on 2020/8/7.
+//  Copyright © 2020 Mars. All rights reserved.
+//
+
+import XCTest
+@testable import Sky
+
+class WeatherDataManagerTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+}
