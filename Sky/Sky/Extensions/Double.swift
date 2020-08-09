135936dd7462dd43384a265b649269fe70882d3b
fengyuxuan2008 <453039687@qq.com>
2020-08-09 21:11:10 +0800

    实现显示当前天气的VC

---
+++ Sky/Sky/Extensions/Double.swift
@@ -0,0 +1,15 @@
+//
+//  Double.swift
+//  Sky
+//
+//  Created by apple on 2020/8/9.
+//  Copyright © 2020 Mars. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func toCelcius() -> Double {
+        return (self - 32.0) / 1.8
+    }
+}
